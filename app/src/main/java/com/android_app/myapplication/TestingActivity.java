cb773b8597189717689679ee433f07c3252314fa
Softwarica College Lab <softwarica@softwarica.com>
2020-01-21 12:40:59 +0545

    Testing Activity Arithmetic  Class

--- app/src/main/java/com/android_app/myapplication/TestingActivity.java
+++ app/src/main/java/com/android_app/myapplication/TestingActivity.java
@@ -3,12 +3,18 @@ package com.android_app.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TestingActivity extends AppCompatActivity {
+    private Button btnSum, btnSubtract, btnDivide;
+    private EditText etFirst, etSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
+
+       btnSum = findViewById(R.id.btnSum);
     }
 }
---
+++ app/src/main/java/com/android_app/myapplication/model/Arithmetic.java
@@ -0,0 +1,13 @@
+package com.android_app.myapplication.model;
+
+public class Arithmetic {
+    public float add(float f, float s){
+        return f + s ;
+    }
+    public float subtract(float f, float s){
+        return f - s ;
+    }
+    public float devide (float f , float s){
+        return f / s ;
+    }
+}
